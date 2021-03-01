a638356977c74f2946a720e232566bd8b71e5cdb
Nguyen Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2021-03-01 09:45:36 +0700

    add file bai1.py

---
+++ bai1.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+
+img = cv.imread("image/image1.jpg")
+cv.imshow("anh goc: ", img)
+cv.waitKey(4)
+
+[w, h] = img.shape[:2]
+for i in range(w):
+    for j in range(h):
+        img[i][j] = 255 - img[i][j]
+cv.imshow("anh am bang", img)
+cv.waitKey()
+# anh am bang la anh 255 - cho cac diem anh
